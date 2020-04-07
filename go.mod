5d94f7438d6633d22fe11198ce77b9281f868939
Louis Barrett <louis.barrett@segment.com>
2020-04-07 10:20:20 -0700

    anubis

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/louisbarrett/anubis-cli
+
+go 1.13
+
+require github.com/Jeffail/gabs/v2 v2.5.0
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+  "bytes"
+  "encoding/json"
+  "flag"
+  "fmt"
+  "io/ioutil"
+  "log"
+  "net/http"
+"strings"
+  "github.com/Jeffail/gabs/v2"
+)
+
+// IPAddress  -
+
+var (
+  IPAddress = flag.String("ip", "", "IP Address")
+  flagMaltego = flag.Bool("maltego", false, "Generate maltego transform response")
+  entityTemplate = `
+	<Entity Type="maltego.Phrase">
+	<Value>DATA</Value>
+	<Weight>100</Weight>
+	</Entity>
+	`
+	maltegoMessageTemplate = `
+	<MaltegoMessage>
+	<MaltegoTransformResponseMessage>
+	<Entities>
+		MALTEGO_ENTITY
+	</Entities>
+	</MaltegoTransformResponseMessage>
+	</MaltegoMessage>
+	`
+)
+
+
+type anubisRequest struct {
+  IPAddress string
+}
+
+
+
+
+func main() {
+  flag.Parse()
+
+  if *IPAddress != "" {
+    httpClient := http.Client{}
+    requestBody, _ := json.Marshal(anubisRequest{IPAddress: *IPAddress})
+    requestURL := "https://intel.malwareroulette.io/anubis/ip"
+    requestReader := bytes.NewReader(requestBody)
+    httpRequest, err := http.NewRequest("POST", requestURL, requestReader)
+    httpRequest.Header.Add("user-agent", "Anubis CLI")
+    httpRequest.Header.Add("Content-Type", "application/json")
+
+    httpResponse, err := httpClient.Do(httpRequest)
+    responseBytes := httpResponse.Body
+    message, err := ioutil.ReadAll(responseBytes)
+    prettyPrint, err := gabs.ParseJSON(message)
+
+    if err != nil {
+      log.Fatal("", string(message), err)
+    }
+    response := string(prettyPrint.String())
+    if !*flagMaltego {
+      fmt.Println(response)
+    } else {
+      RiskRating := prettyPrint.Search("RiskRating").Data().(string)
+      ResponseEntity := strings.Replace(entityTemplate,"DATA",RiskRating,1)
+      ResponseMessage := strings.Replace(maltegoMessageTemplate,"MALTEGO_ENTITY",ResponseEntity,1)
+      fmt.Println(ResponseMessage)
+    }
+  } else {
+    flag.Usage()
+  }
+
+}
