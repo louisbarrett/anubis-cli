ca2cd9190cea81191f29b98baacad45af71e1a36
Louis Barrett <45020586@users.noreply.github.com>
2020-05-15 09:51:14 -0700

    Updated maltego output for entity to anubis.Risk

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-  "bytes"
-  "encoding/json"
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "log"
-  "net/http"
-"strings"
-  "github.com/Jeffail/gabs/v2"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Jeffail/gabs/v2"
 )
 
 // IPAddress  -
 
 var (
-  IPAddress = flag.String("ip", "", "IP Address")
-  flagMaltego = flag.Bool("maltego", false, "Generate maltego transform response")
-  entityTemplate = `
-	<Entity Type="maltego.Phrase">
+	IPAddress      = flag.String("ip", "", "IP Address")
+	flagMaltego    = flag.Bool("maltego", false, "Generate maltego transform response")
+	entityTemplate = `
+	<Entity Type="anubis.Risk">
 	<Value>DATA</Value>
 	<Weight>100</Weight>
 	</Entity>
@@ -34,45 +35,41 @@ var (
 	`
 )
 
-
 type anubisRequest struct {
-  IPAddress string
+	IPAddress string
 }
 
-
-
-
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  if *IPAddress != "" {
-    httpClient := http.Client{}
-    requestBody, _ := json.Marshal(anubisRequest{IPAddress: *IPAddress})
-    requestURL := "https://intel.malwareroulette.io/anubis/ip"
-    requestReader := bytes.NewReader(requestBody)
-    httpRequest, err := http.NewRequest("POST", requestURL, requestReader)
-    httpRequest.Header.Add("user-agent", "Anubis CLI")
-    httpRequest.Header.Add("Content-Type", "application/json")
+	if *IPAddress != "" {
+		httpClient := http.Client{}
+		requestBody, _ := json.Marshal(anubisRequest{IPAddress: *IPAddress})
+		requestURL := "https://intel.malwareroulette.io/anubis/ip"
+		requestReader := bytes.NewReader(requestBody)
+		httpRequest, err := http.NewRequest("POST", requestURL, requestReader)
+		httpRequest.Header.Add("user-agent", "Anubis CLI")
+		httpRequest.Header.Add("Content-Type", "application/json")
 
-    httpResponse, err := httpClient.Do(httpRequest)
-    responseBytes := httpResponse.Body
-    message, err := ioutil.ReadAll(responseBytes)
-    prettyPrint, err := gabs.ParseJSON(message)
+		httpResponse, err := httpClient.Do(httpRequest)
+		responseBytes := httpResponse.Body
+		message, err := ioutil.ReadAll(responseBytes)
+		prettyPrint, err := gabs.ParseJSON(message)
 
-    if err != nil {
-      log.Fatal("", string(message), err)
-    }
-    response := string(prettyPrint.String())
-    if !*flagMaltego {
-      fmt.Println(response)
-    } else {
-      RiskRating := prettyPrint.Search("RiskRating").Data().(string)
-      ResponseEntity := strings.Replace(entityTemplate,"DATA",RiskRating,1)
-      ResponseMessage := strings.Replace(maltegoMessageTemplate,"MALTEGO_ENTITY",ResponseEntity,1)
-      fmt.Println(ResponseMessage)
-    }
-  } else {
-    flag.Usage()
-  }
+		if err != nil {
+			log.Fatal("", string(message), err)
+		}
+		response := string(prettyPrint.String())
+		if !*flagMaltego {
+			fmt.Println(response)
+		} else {
+			RiskRating := prettyPrint.Search("RiskRating").Data().(string)
+			ResponseEntity := strings.Replace(entityTemplate, "DATA", RiskRating, 1)
+			ResponseMessage := strings.Replace(maltegoMessageTemplate, "MALTEGO_ENTITY", ResponseEntity, 1)
+			fmt.Println(ResponseMessage)
+		}
+	} else {
+		flag.Usage()
+	}
 
 }
